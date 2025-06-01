3147158e8220327a542dd40383191e4f33e6d034
clarissa <92129914+clarsbyte@users.noreply.github.com>
2025-06-01 13:24:20 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState, useNavigate } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import supabase from './supabaseClient'
 
